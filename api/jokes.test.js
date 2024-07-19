309b6923d0acc7f3bd3d54b2649bb2211acd82af
Tito2point0 <defconx00@gmail.com>
2024-07-19 19:12:16 -0400

    4 tests written 4 test passing working on a deleting jokes. basics test done

--- api/jokes.test.js
+++ api/jokes.test.js
@@ -1,18 +1,53 @@
 const request = require("supertest");
 const db = require("../db-config")
 const server = require("../server");
+const Joke = require("./jokesModel")
 
 
 
 const joke1 = { joke: "Why did the scarecrow win an award?", punchline: "Because he was outstanding in his field." }
 const joke2 = { joke: "Why did the chicken cross the road?", punchline: "Because he was free range." }
 
-beforeAll(async () => {
-    await db.migrate.rollback()
-    await db.migrate.latest()
+// beforeAll(async () => {
+//     await db.migrate.rollback()
+//     await db.migrate.latest()
+// })
+beforeEach(async () => {
+    await db("jokes").truncate()
 })
 
-it('            correct env var', () => {
+afterAll(async () => {
+
+    await db.destroy()
+})
+it('correct env var', () => {
         expect(process.env.NODE_ENV).toBe('test')
 })
 
+
+describe("jokes model functions", () => {
+    describe("creates joke", () => {
+        it("adds a joke to the db", async () => {
+            let jokes
+            await Joke.createJoke(joke1)
+            jokes = await db("jokes")
+            expect(jokes).toHaveLength(1)
+            
+            await Joke.createJoke(joke2)
+            jokes = await db("jokes")
+            expect(jokes).toHaveLength(2)
+
+        })
+        it("inserted joke and punchline ", async() => {
+            const joke = await Joke.createJoke(joke1) 
+            expect(joke).toMatchObject({joke_id:1,...joke})
+        })
+    })
+    describe("[DELETE] / - deleting joke", () => {
+        it("removes joke from database", async () => {
+            const [joke_id] = await db("jokes").insert(joke1)
+        })
+    
+})
+
+})
---
+++ api/jokesModel.js
@@ -0,0 +1,12 @@
+const db = require("../db-config")
+
+
+async function createJoke(joke) {
+    const [id] = await db("jokes").insert(joke)
+    return db("jokes").where("joke_id", id).first()
+
+}
+
+module.exports = {
+    createJoke
+}
